0ff8c83dfba2ab9f2e7b4ec4a091e7c721036481
caden <cpete.est2014@gmail.com>
2023-08-31 09:55:57 -0600

    started game

---
+++ BrickBreaker/Assets/Scripts/BrickBehaviour.cs
@@ -0,0 +1,15 @@
+
+using System;
+using UnityEngine;
+
+public class BrickBehaviour : MonoBehaviour
+{
+   public BrickData brickHealth;
+   public BrickData brickPoints;
+   public BrickData brickColor;
+
+   private void OnCollisionEnter(Collision collision)
+   {
+      
+   }
+}
---
+++ BrickBreaker/Assets/Scripts/BrickData.cs
@@ -0,0 +1,9 @@
+//assisted by ChatGPT
+using UnityEngine;
+[CreateAssetMenu]
+public class BrickData : ScriptableObject
+{
+    public int points = 10;
+    public int health = 1;
+    public Color brickColor = Color.white;
+}
