a4513f29fe9b6d69b11fc1d489fc15b3a0372c4d
caden <cpete.est2014@gmail.com>
2023-09-21 13:02:27 -0600

    brick positioning working

---
+++ BrickBreaker/Assets/Scripts/BrickPosList.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class BrickPosList : ScriptableObject
+{
+    public List<Vector3> positions;
+}
--- BrickBreaker/Assets/Scripts/BrickSpawnBehavior.cs
+++ BrickBreaker/Assets/Scripts/BrickSpawnBehavior.cs
@@ -6,19 +6,17 @@ public class BrickSpawnBehavior : MonoBehaviour
 {
     public GameObject brickPrefab;
     public IntData numBricks;
+    public BrickPosList[] brickPositionsOptions;
 
     public void SpawnBricks()
     {
-        for(int i = 0; i < numBricks.value; i++) 
+        BrickPosList positions = brickPositionsOptions[Random.Range(0, brickPositionsOptions.Length)];
+       
+        for(int i = 0; i < numBricks.value; i++)
         {
-            Vector3 randomPosition = new Vector3
-            (
-                Random.Range(-8f, 8f), 
-                Random.Range(0, 5.3f), 
-                0f
-            );
+            Vector3 position = positions.positions[i];
 
-            Instantiate(brickPrefab, randomPosition, Quaternion.identity);
+            Instantiate(brickPrefab, position, Quaternion.identity);
         }
     }
 }
